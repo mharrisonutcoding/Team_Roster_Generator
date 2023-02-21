59909074d93c4f094bcedeea42fb88708ce26c2f
Mike Harrison <mharrisonutcoding@gmail.com>
2023-02-20 20:36:59 -0600

    added employee test cases

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -6,3 +6,20 @@ test("Will constructor properly set", () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test("Should get the employee name"), () => {
+    const employee = new Employee('Name', 10, 'testing@email.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+};
+test("Should get the employee ID"), () => {
+    const employee = new Employee('Name', 10, 'testing@email.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+};
+test("Should get the employee email"), () => {
+    const employee = new Employee('Name', 10, 'testing@email.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+};
+test("Should get the employee role"), () => {
+    const employee = new Employee('Name', 10, 'testing@email.com');
+    expect(employee.getRole()).toEqual("Employee");
+};
