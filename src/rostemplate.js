d123bc26407429ec3d54d2c107a5425a7630c32f
Mike Harrison <mharrisonutcoding@gmail.com>
2023-02-27 19:33:48 -0600

    added html template for rendering

--- src/rostemplate.js
+++ src/rostemplate.js
@@ -58,4 +58,36 @@ const renderIntern = function (intern) {
     `;
 };
 
+const generateTeamRoster = function () {   
+    return`
+          <!DOCTYPE html>
+          <html lang="en">
+              <head>
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <title>Team-Roster</title>
+                  <link rel="stylesheet" href="style.css">
+              </head>
+              <body>
+                  <header>
+                      <nav class="navbar" id="navbar">
+                          <span class="navbar-brand mb-1 h1 w-100 text-center" id="navbar-text">Team Roster</span>
+                      </nav>
+                  </header>
+                  <main>
+                      <div class="container">
+                          <div class="row justify-content-center" id="team-cards">
+                              <!--Team Roster Cards-->
+                              
+                          </div>
+                      </div>
+                  </main>
+              </body>
+              <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+              <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+          </html>
+      `;
+  };
 
+  // export to index
+  module.exports = generateHTML;
