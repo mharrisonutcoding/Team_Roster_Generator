aec0bca6d837591bfef2f3772bfccb37bff7a1fe
Mike Harrison <mharrisonutcoding@gmail.com>
2023-02-27 18:36:39 -0600

    all test cases pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,17 +2,17 @@ const Intern = require("../lib/Intern");
   
 
 
-testing("Will constructor properly set", () => {
+test("Will constructor properly set", () => {
     const intern = new Intern('Name', 10, 'testing@email.com', 'school');
     expect(intern.school) .toEqual(expect.any(String));
 });
 
-testing("Should get interns school", () => {
+test("Should get interns school", () => {
     const intern = new Intern('Name', 10, 'testing@email.com', 'school');
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
-testing("Should get role", () => {
+test("Should get role", () => {
     const intern = new Intern('Name', 10, 'testing@email.com', 'school');
     expect(intern.getRole()).toEqual("Intern");
 }); 
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const fs = require("fs");
 
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern")
+const Intern = require("./lib/intern");
